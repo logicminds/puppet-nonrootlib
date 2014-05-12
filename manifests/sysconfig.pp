a914cf92fb746541ebbd286a201f39538ebd9dc5
Corey Osman <corey@logicminds.biz>
2014-05-12 16:04:26 -0700

    abstracted some of the file attritubutes to parameters

--- manifests/sysconfig.pp
+++ manifests/sysconfig.pp
@@ -3,12 +3,14 @@
 define nonrootlib::sysconfig_file(
   $sysconfig_file_name = $name,
   $data = {}
+  $template = '<%= @data.each { |key, value| "#{key}=#{value}\n" } %>'
+  $ensure_value = 'present'
 ){
 
   $sysconfig_file_path  = "${nonrootlib::sysconfig_dir}/${sysconfig_file_name}"
 
   file{$sysconfig_file_path:
-    ensure => present,
-    content => '<%= @data.each { |key, value| "#{key}=#{value}\n" } %>'
+    ensure => $ensure_value,
+    content => $template,
   }
 }
